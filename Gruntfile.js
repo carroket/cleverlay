54adf2574bb2bf5e73e1ce59b0dc978997d6c07f
Brian Sexton <brian.sexton@gmail.com>
2016-06-28 23:51:56 -0700

    Add clean and copy tasks for Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,30 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+
+		clean: {
+			build: {
+				src: "build"
+			}
+		},
+
+		copy: {
+			build: {
+				files: [
+					{
+						expand: true,
+						cwd: 'source/',
+						src: ['cleverlay.css', 'cleverlay.js'],
+						dest: 'build/'
+					}
+				]
+			}
+		}
 	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
 	grunt.registerTask('default', []);
 };
